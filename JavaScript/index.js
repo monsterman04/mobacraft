d4b4f00f6d857508786153dcd8c7f32120bfb0c4
monsterman04 <79773093+monsterman04@users.noreply.github.com>
2022-09-10 23:59:48 +0200

    Working messaging

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -9,3 +9,28 @@ function type(){
         setTimeout(type, speed);
     }
 }
+
+// ↑ Typewriter ↑
+
+// ↓ Form sender ↓
+
+let form = document.querySelector("form");
+let button = document.querySelector("#form-button");
+form.addEventListener('submit', (e) =>{
+    button.innerText = "Sending...";
+    e.preventDefault();
+    let data = new FormData(form);
+    fetch('https://script.google.com/macros/s/AKfycbz_NRYhKjZ_FgCUieSjmrKse_-kqA5DrXI4D0w8zHz-q-U1K_bMVoR1ibPJrrxbV_EN_Q/exec', {
+        method: "POST",
+        body: data
+    })
+    .then(res => res.text())
+    .then(data => onSubmit(data));
+})
+
+function onSubmit(data){
+    button.innerText = "✔";
+    console.log(data);
+    form.reset();
+    setTimeout(() => button.innerText = "Send", 5000);
+}
